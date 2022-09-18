0ade282af66778f2f555d98aee9fd14c2216fd64
kubragunesy <kubragunes76@gmail.com>
2022-09-18 23:49:19 +0300

    update

---
+++ YildizWebProject/YildizWebProject/Controllers/BusinessController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YildizWebProject.Models.Entity;
+using EntitiyLayer.Concrete;
+
+namespace YildizWebProject.Controllers
+{
+    public class BusinessController : Controller
+    {
+        // GET: Business
+        BusinessManager businessManager = new BusinessManager(new EfBusinessDal());
+        public ActionResult Index()
+        {
+            var degerler = businessManager.GetAll();
+            return View(degerler);
+        }
+    }
+}
