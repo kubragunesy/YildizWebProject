ae93b11d7088aed9c1f3212d9696894f7889f80b
kubragunesy <kubragunes76@gmail.com>
2022-10-11 20:41:31 +0300

    update

--- YildizWebProject/EntitiyLayer/Concrete/Business.cs
+++ YildizWebProject/EntitiyLayer/Concrete/Business.cs
@@ -13,6 +13,7 @@ namespace EntitiyLayer.Concrete
         public int businessId { get; set; }
         public string businessName { get; set; }
         public string businessDescription { get; set; }
+        public string businessIcon { get; set; }
         public bool statu { get; set; }
     }
 
