511ece49149926d637183c7d5255fe63be691eb9
kubragunesy <kubragunes76@gmail.com>
2022-10-18 00:35:08 +0300

    UPDATE

--- YildizWebProject/Controllers/HomeController.cs
+++ YildizWebProject/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace YildizWebProject.Controllers
@@ -21,10 +22,18 @@ namespace YildizWebProject.Controllers
         ProjectManager projectManager = new ProjectManager(new EfProjectDal());
         CompanyInfoManager companyInfoManager = new CompanyInfoManager(new EfCompanyInfoDal());
 
+        [HttpGet]
         public ActionResult Index()
         {
             
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Contact contact)
+        {
+
+            contactManager.Insert(contact);
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
@@ -42,6 +51,40 @@ namespace YildizWebProject.Controllers
         [HttpPost]
         public ActionResult Contact(Contact contact)
         {
+            //if (contact.customerName!=null && contact.customerLastName!=null && contact.customerMail!=null && contact.customerPhoneNumber!=null && contact.customerMessage!=null)
+            //{
+            //    WebMail.SmtpServer = "smpt.gmail.com";
+            //    WebMail.EnableSsl= true;
+            //    WebMail.UserName = "insaatisii@gmail.com";
+            //    WebMail.Password = "insaat61";
+            //    WebMail.SmtpPort = 587;
+            //    WebMail.Send("insaatisii@gmail.com", contact.customerMail, contact.customerName, contact.customerLastName + "</br>" +  contact.customerMessage);
+            //    ViewBag.Uyari = "Mesajınız başarıyla gönderilmiştir";
+            //}
+            //else
+            //{
+            //    ViewBag.Uyari = "Hata Oluştu. Tekrar Deneyiniz";
+            //}
+            
+
+            //MailMessage mail = new MailMessage();
+            //mail.IsBodyHtml = true;
+            //mail.To.Add("insaatisii@gmail.com"); //mail gönderilen adres
+            //mail.From = new MailAddress(contact.customerMail); //maili gönderen adres
+            //mail.Subject = "İletişim Formu";
+            //string adsoyad = "İsim Soyism : " + contact.customerName + contact.customerLastName+ "adsoyad" + "<br/>";
+            //string eposta = "E-Posta Adresi : " + contact.customerMail+ "email" + "<br/>";
+            //string ceptel = "Cep Telefonu : " + contact.customerPhoneNumber + "gsm" + "<br/>";
+            //string mesaj = "Mesaj : " + contact.customerMessage +"mesaj" + "<br/>";
+            //string Body = adsoyad + eposta + ceptel + mesaj;
+            //mail.Body = Body;
+            //SmtpClient smtp = new SmtpClient(); 
+            //smtp.Host = "smtp.gmail.com"; //mail serverının host bilgisi
+            //smtp.Port = 587; //mail serverının portu
+            //smtp.UseDefaultCredentials = false;
+            //smtp.Credentials = new System.Net.NetworkCredential("insaatisii@gmail.con", "insaat61"); //mail serverının kullanıcı bilgileri
+            //smtp.Send(mail);
+            
             //MailMessage mail=new MailMessage();
             //mail.To.Add("abdussametsolak@outlook.com"); // kıme
             //mail.From = new MailAddress("abdussametsolak@outlook.com"); //kımden
@@ -68,7 +111,7 @@ namespace YildizWebProject.Controllers
             //    TempData["Message"]="mail gonderılemedi "+ex.Message;
 
             //}
-            
+
             contactManager.Insert(contact);
             return RedirectToAction("Contact");
         }
